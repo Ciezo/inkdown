2a36f99c34b53fd2a0a8cd62ad091a041a727bff
Cloyd Van <abcd123.letters@gmail.com>
2023-06-14 23:23:49 +0800

    NotesAPI
    A class that encapsulates methods for creating, fetching, updating, and deleting notes contents
    headers for redirection are set properly
    and an error page is added to handle error viewing

---
+++ api/NotesAPI.php
@@ -0,0 +1,78 @@
+<?php 
+
+class NotesAPI {
+
+    public static function createNote($user_id, $author, $title, $body, $date_posted) {
+        $sql = "INSERT INTO NOTES (user_id, author, title, body, date_posted)
+                    VALUES 
+                    (
+                        '$user_id',
+                        '$author',
+                        '$title',
+                        '$body',
+                        '$date_posted'
+                    )";
+        
+        require("../../config.php");
+        if (mysqli_query($conn, $sql)) {
+            header("location: ../user/home.php");
+        } else {
+            header("location: ../error/error.php");
+        }
+    }
+
+    public static function getNotesList() {
+        $sql = "SELECT * FROM NOTES";
+        $notes_ls = array();
+        
+        require("../../config.php");
+        $results = mysqli_query($conn, $sql);
+        while ($rows = mysqli_fetch_assoc($results)) {
+            $notes_ls[] = $rows;
+        }
+            
+        return $notes_ls; 
+    }
+    
+    public static function getNotesListByUserID($user_id) {
+        $sql = "SELECT * FROM NOTES WHERE user_id = '$user_id'";
+        $notes_ls_byID = array();
+        
+        require("../../config.php");
+        $results = mysqli_query($conn, $sql);
+        while ($rows = mysqli_fetch_assoc($results)) {
+            $notes_ls_byID[] = $rows;
+        }
+            
+        return $notes_ls_byID; 
+    }
+
+    public static function updateNote($note_id, $user_id, $title, $body, $date_posted) {
+        $sql = "UPDATE NOTES SET 
+                    title = '$title',
+                    body = '$body',
+                    date_posted = '$date_posted'
+                WHERE note_id = '$note_id' AND user_id = '$user_id'";
+        
+        require("../../config.php");
+        if (mysqli_query($conn, $sql)) {
+            Print '<script>alert("Contents updated!");</script>';
+        } else {
+            header("location: ../error/error.php");
+        }
+    }
+
+    public static function deleteNote($note_id, $user_id) {
+        $sql = "DELETE FROM NOTES WHERE note_id = '$note_id' AND user_id = '$user_id'";
+        
+        require("../../config.php");
+        if (mysqli_query($conn, $sql)) {
+
+        } else {
+            header("location: ../error/error.php");
+        }
+    } 
+
+}
+
+?>
