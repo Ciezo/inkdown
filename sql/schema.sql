4aa44df78c2b6f16d2278cdbd13410ecaf1f025b
Cloyd Van <abcd123.letters@gmail.com>
2023-06-15 15:53:06 +0800

    Created trash table for archiving notes

--- sql/schema.sql
+++ sql/schema.sql
@@ -30,3 +30,16 @@ CREATE TABLE IF NOT EXISTS NOTES(
     PRIMARY KEY (note_id),
     FOREIGN KEY (user_id) REFERENCES USERS(user_id)
 );
+
+CREATE TABLE IF NOT EXISTS TRASH(
+    trash_id    INT                 NOT NULL auto_increment,
+    note_id     INT                 NOT NULL, 
+    user_id     INT                 NOT NULL, 
+    author      VARCHAR(255)        NOT NULL,
+    title       VARCHAR(255)        NOT NULL,
+    body        LONGTEXT            NOT NULL,
+    date_posted VARCHAR(100)        NOT NULL,
+
+    PRIMARY KEY (trash_id),
+    FOREIGN KEY (user_id) REFERENCES USERS(user_id)
+);
